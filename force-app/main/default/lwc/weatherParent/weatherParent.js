f10983f0531a9f93b97d7f35b07d8414e400c8b6
droy-aair <86105643+droy-aair@users.noreply.github.com>
2022-05-01 23:21:32 +0530

    weatherInit
    called apex from lwc

---
+++ force-app/main/default/lwc/weatherParent/weatherParent.js
@@ -0,0 +1,39 @@
+import { LightningElement } from 'lwc';
+import apiCaller from '@salesforce/apex/WeatherController.apiCaller';
+import apiCaller2 from '@salesforce/apex/WeatherController.apiCaller2';
+import apiCaller3 from '@salesforce/apex/WeatherController.apiCaller3';
+
+export default class WeatherParent extends LightningElement {
+
+    weatherResult = 'testResult';
+    weatherError = '';
+
+    connectedCallback(){
+       
+                    this.weatherApiCaller();
+       
+    }
+
+
+
+    weatherApiCaller(){
+        console.log('weatherApiCaller----');
+        try{
+
+            apiCaller({apiInput:'apiInput'}).then(result=>{
+                this.weatherResult = result;
+                console.log(result);
+                console.log(JSON.stringify(result));
+            }).catch(error=>{
+                this.weatherError = error;
+                console.log('-----error-----');
+                console.log(error);
+            })
+            console.log('-weatherApiCaller-');
+        }  
+        catch(e){
+            console.log('weatherApiCaller-----error-----weatherApiCaller');
+            console.log(e);
+        }
+    }
+}
